a6462c8c8addd4924bacb971c562de17a7552630
Casey <caseymorris61@gmail.com>
2020-04-19 14:35:07 -0500

    Add agents page with filter

--- app/main.py
+++ app/main.py
@@ -4,19 +4,24 @@ import sqlite3
 #initializations
 app = Flask(__name__)
 
+# Path for Heroku
 DATABASE='/app/app/static/agentdatabase'
+
+# Path for local
+# DATABASE='static/agentdatabase'
 
 @app.route("/")
 @app.route("/index")
 def main():
     playersInfo = []
-    playerquery = "select name, agentID, salary from players"
+    playerquery = "select players.name, players.agentID, players.salary, certified_agents.name from players join certified_agents on players.agentID=certified_agents.agentID"
     c = get_db().cursor()
     c.execute(playerquery)
     players = c.fetchall()
     for row in players:
         name = row[0]
         agentID = row[1]
+        agentName = row[3]
         try:
             salaryVal = int(row[2].strip('"').strip('$').replace(',',"").strip())
         except ValueError :
@@ -24,15 +29,15 @@ def main():
 
         salary = '${:,.2f}'.format(salaryVal)
 
-        d = [name,agentID,salary,salaryVal]
+        d = [name,agentID,salary,agentName,salaryVal]
         playersInfo.append(d)
 
 
-    playersInfo.sort(key=lambda x: x[3],reverse=True)
+    playersInfo.sort(key=lambda x: x[4],reverse=True)
 
     return render_template('index.html', players=playersInfo)
 
-@app.route('/agent/<variable>', methods=['GET'])
+@app.route('/agents/<variable>', methods=['GET'])
 def get_agent(variable):
 
     agentquery = "select name, address, agency, email, phone, mobile, url_ext from certified_agents where agentID = ?"
@@ -48,6 +53,26 @@ def get_agent(variable):
     mobile = agent[5]
     url_ext = agent[6]
     return render_template('agent.html', name=name,address=address,agency=agency,email=email,phone=phone,mobile=mobile,url_ext=url_ext)
+
+@app.route("/agents")
+def agents():
+    agentsquery = "select agentID, name, email, phone, mobile from certified_agents"
+    c = get_db().cursor()
+    c.execute( agentsquery)
+    agentsRes = c.fetchall()
+
+    agents = []
+    for row in agentsRes:
+        aid = str(row[0])
+        name = row[1]
+        email = row[2]
+        phone = row[3]
+        mobile = row[4]
+
+        a = [aid,name,email,phone,mobile]
+        agents.append(a)
+
+    return render_template('agents.html',agents=agents)
 
 def get_db():
     db = getattr(g, '_database', None)
